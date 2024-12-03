6bb641216ef4c2a58da81afe429afad4f1212a5e
Anna Suuronen <annasuuronen@icloud.com>
2024-12-03 16:18:05 +0200

    tehtävä 4.2 tehty

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger =  require('./utils/logger')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
