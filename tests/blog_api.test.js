d06f28fb12ff7802ba1824712803e218d473121e
Anna Suuronen <annasuuronen@icloud.com>
2024-12-04 02:07:27 +0200

    tehtävä 4.11 tehty

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -31,80 +31,125 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('testing get method', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('the right amount of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
+
+  test('the first blog has right information', async () => {
+    const response = await api.get('/api/blogs')
+
+    const firstBlog = response.body[0]
+    assert.strictEqual(initialBlogs[0].title, firstBlog.title)
+    assert.strictEqual(initialBlogs[0].author, firstBlog.author)
+    assert.strictEqual(initialBlogs[0].url, firstBlog.url)
+  })
+
+  test('blogs have id, not _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const firstBlog = response.body[0]
+    const secondBlog = response.body[1]
+    assert("id" in firstBlog)
+    assert("id" in secondBlog)
+  })
 })
 
-test('the right amount of blogs is returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('testing post method', () => {
+  test('A blog can be added', async () => {
+    const newBlog = {
+      title: "Uusi aihe",
+      author: "Uusi kirjoittaja",
+      url: "wwww.uusi.com",
+      likes: 1
+    }
 
-  assert.strictEqual(response.body.length, initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const blogs = response.body.map(blog => blog.title)
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+    assert(blogs.includes('Uusi aihe'))
+  })
+
+  test('Added blog has the right attributes', async () => {
+    const newBlog = {
+      title: "Uusi aihe",
+      author: "Uusi kirjoittaja",
+      url: "wwww.uusi.com",
+      likes: 1
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const blog = response.body.find(blog => blog.title === newBlog.title)
+
+    assert('title' in blog)
+    assert('author' in blog)
+    assert('url' in blog)
+    assert('id' in blog)
+    assert('likes' in blog)
+  })
 })
 
-test('the first blog has right information', async () => {
-  const response = await api.get('/api/blogs')
+describe('testing likes validation', () => {
+  test('if likes are not given, likes will be set to 0', async () => {
+    const newBlog = {
+      title: "Uusi aihe",
+      author: "Uusi kirjoittaja",
+      url: "wwww.uusi.com"
+    }
 
-  const firstBlog = response.body[0]
-  assert.strictEqual(initialBlogs[0].title, firstBlog.title)
-  assert.strictEqual(initialBlogs[0].author, firstBlog.author)
-  assert.strictEqual(initialBlogs[0].url, firstBlog.url)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const blog = response.body.find(blog => blog.title === newBlog.title)
 
-test('blogs have id, not_id', async () => {
-  const response = await api.get('/api/blogs')
+    assert.strictEqual(blog.likes, 0)
+  })
 
-  const firstBlog = response.body[0]
-  const secondBlog = response.body[1]
-  assert("id" in firstBlog)
-  assert("id" in secondBlog)
-})
+  test('if likes are give, likes will not be set to 0', async () => {
+    const newBlog = {
+      title: "Uusi aihe",
+      author: "Uusi kirjoittaja",
+      url: "wwww.uusi.com",
+      likes: 10
+    }
 
-test('A blog can be added', async() => {
-  const newBlog = {
-    title: "Uusi aihe",
-    author: "Uusi kirjoittaja",
-    url: "wwww.uusi.com",
-    likes: 1
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const blog = response.body.find(blog => blog.title === newBlog.title)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  
-  const response = await api.get('/api/blogs')
-  const blogs = response.body.map(blog => blog.title)
-
-  assert.strictEqual(response.body.length, initialBlogs.length + 1)
-  assert(blogs.includes('Uusi aihe'))
-})
-
-test('Added blog has the right attributes', async() => {
-  const newBlog = {
-    title: "Uusi aihe",
-    author: "Uusi kirjoittaja",
-    url: "wwww.uusi.com",
-    likes: 1
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  
-  const response = await api.get('/api/blogs')
-  const blog = response.body.find(blog => blog.title === newBlog.title)
-
-  assert('title' in blog)
-  assert('author' in blog)
-  assert('url' in blog)
-  assert('id' in blog)
-  assert('likes' in blog)
+    assert.strictEqual(blog.likes, 10)
+  })
 })
 
 after(async () => {
