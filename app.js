536014a40a740382cbb01c69c7ed49af20b33276
Anna Suuronen <annasuuronen@icloud.com>
2024-12-05 04:34:11 +0200

    tehtävä 4.22 tehty

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
+//app.use(middleware.userExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,15 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+blogsRouter.post('/', userExtractor, async (request, response, next) => {
+  const user = request.user
+  
   const body = request.body
   const blog = new Blog({
     title: body.title,
@@ -31,19 +26,12 @@ blogsRouter.post('/', async (request, response, next) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
   const id = request.params.id
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blogGettingDeleted = await Blog.findById(id)
-  console.log(user)
-  console.log(blogGettingDeleted)
+
   if (user._id.toString() === blogGettingDeleted.user.toString()) {
     await Blog.findByIdAndDelete(id)
     response.status(204).end()
